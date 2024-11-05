43206c5e5ee23faa59004ae773767d6522a991fe
Nurulfarhana <DEVELOPER@CMSBPC02ACER>
2024-11-05 11:45:40 +0800

    commit

---
+++ PBTPro.Api/PBTPro.Api/Program.cs
@@ -0,0 +1,175 @@
+using Microsoft.EntityFrameworkCore;
+using PBTPro.Api.Services;
+using PBTPro.Shared.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using PBTPro.DAL;
+using Prometheus;
+using System.Reflection;
+using System.Text;
+using System.Text.Json.Serialization;
+using PBTPro.Api.Controllers.Base;
+using PBTPro.Api.Controllers;
+
+var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+IConfiguration configuration = builder.Configuration;
+
+// Add services to the container.
+
+// Register the configuration for IConfiguration
+builder.Services.AddSingleton<IConfiguration>(configuration);
+
+// Register the controller
+builder.Services.AddScoped<LotController>();
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddControllers();
+
+#region DB
+builder.Services.AddDbContext<PBTProDbContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"), x => x.UseNetTopologySuite()));
+#endregion
+
+#region Interface
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(swagger =>
+{
+    swagger.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "PBTPro Web API",
+        Description = "PBTPro Web API, build: " + Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+    });
+    //swagger.DocumentFilter<SwaggerAPIPathPrefixInserter>("/web-api");
+    swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\"",
+    });
+    swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                     Type = ReferenceType.SecurityScheme,
+                     Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+builder.Services.AddControllersWithViews().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.NumberHandling = JsonNumberHandling.AllowNamedFloatingPointLiterals;
+    //options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+    //options.JsonSerializerOptions.PropertyNamingPolicy = null; // prevent camel case
+
+    // Register the custom GeoJSON converter
+    options.JsonSerializerOptions.Converters.Add(new GeometryJsonConverter());
+});
+#endregion
+
+#region Utilities
+//Email Sender
+var emailConfig = builder.Configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>();
+builder.Services.AddSingleton(emailConfig);
+builder.Services.AddScoped<IEmailSender, EmailSender>();
+
+//Hosted Background Services Manager for long run Process
+builder.Services.AddSingleton<PBTProBkgdSM>();
+builder.Services.AddHostedService(provider => provider.GetRequiredService<PBTProBkgdSM>());
+#endregion
+
+#region Security
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(MyAllowSpecificOrigins,
+    builder =>
+    {
+        builder.WithOrigins(
+            "http://127.0.0.1",
+            "https://127.0.0.1",
+            "http://localhost",
+            "https://localhost",
+            "http://pbtpro.com.my",
+            "https://pbtpro.com.my")
+        .SetIsOriginAllowed(_ => true)
+        .AllowCredentials().AllowAnyHeader().AllowAnyMethod();
+        //.SetIsOriginAllowedToAllowWildcardSubdomains();
+    });
+});
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<PBTProDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.Events = new JwtBearerEvents();
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidAudience = builder.Configuration["JWT:ValidAudience"],
+        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"])),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+builder.Services.AddScoped<JWTTokenService>();
+builder.Services.AddAuthorization();
+#endregion
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.UseHttpMetrics();
+
+
+//app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapControllerRoute("default", "Web api");
+});
+
+app.Run();
