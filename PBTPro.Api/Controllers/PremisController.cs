ddfafd2cea6ef361c6688f9c7bd1c5d0ce6e1929
Muhammad Ismail <spy.crzy@gmail.com>
2024-12-05 10:09:49 +0800

    sample api for get premis on point click

--- PBTPro.Api/Controllers/PremisController.cs
+++ PBTPro.Api/Controllers/PremisController.cs
@@ -19,6 +19,7 @@ using PBTPro.DAL;
 using PBTPro.DAL.Models;
 using PBTPro.DAL.Models.CommonServices;
 using PBTPro.DAL.Models.PayLoads;
+using System;
 using System.Reflection;
 using System.Text;
 
@@ -31,6 +32,7 @@ namespace PBTPro.Api.Controllers
         private readonly ILogger<PremisController> _logger;
         private readonly string _feature = "PREMIS";
         private readonly int _defCRS = 4326;
+        private static Random random = new Random(); //just to create random dummy data, will need to remove this later
 
         public PremisController(PBTProDbContext dbContext, ILogger<PremisController> logger) : base(dbContext)
         {
@@ -57,17 +59,16 @@ namespace PBTPro.Api.Controllers
                 {
                     gid = x.gid,
                     lot = x.lot,
-                    cukai_status = x.tempoh_sah_cukai == null
-                                ? "tiada"
+                    status_cukai = x.tempoh_sah_cukai == null
+                                ? "None"
                                 : x.tempoh_sah_cukai > DateOnly.FromDateTime(DateTime.Now)
-                                    ? "hidup"
-                                    : "mati",
-
-                    lesen_status = x.tempoh_sah_lesen == null
-                                ? "tiada"
+                                    ? "Active"
+                                    : "Expired",
+                    status_lesen = x.tempoh_sah_lesen == null
+                                ? "None"
                                 : x.tempoh_sah_lesen > DateOnly.FromDateTime(DateTime.Now)
-                                    ? "hidup"
-                                    : "mati",
+                                    ? "Active"
+                                    : "Expired",
 
                     geom = PostGISFunctions.ParseGeoJsonSafely(PostGISFunctions.ST_AsGeoJSON(x.geom)),
                 })
@@ -116,17 +117,16 @@ namespace PBTPro.Api.Controllers
                 {
                     gid = x.gid,
                     lot = x.lot,
-                    cukai_status = x.tempoh_sah_cukai == null
-                                ? "tiada"
+                    status_cukai = x.tempoh_sah_cukai == null
+                                ? "None"
                                 : x.tempoh_sah_cukai > DateOnly.FromDateTime(DateTime.Now)
-                                    ? "hidup"
-                                    : "mati",
-
-                                        lesen_status = x.tempoh_sah_lesen == null
-                                ? "tiada"
+                                    ? "Active"
+                                    : "Expired",
+                    status_lesen = x.tempoh_sah_lesen == null
+                                ? "None"
                                 : x.tempoh_sah_lesen > DateOnly.FromDateTime(DateTime.Now)
-                                    ? "hidup"
-                                    : "mati",
+                                    ? "Active"
+                                    : "Expired",
 
                     geom = PostGISFunctions.ParseGeoJsonSafely(PostGISFunctions.ST_AsGeoJSON(x.geom)),
                 })
@@ -146,7 +146,95 @@ namespace PBTPro.Api.Controllers
             }
         }
 
+
+        [HttpGet]
+        [Route("GetPremisInfo")]
+        public async Task<IActionResult> GetPremisInfo(int gid)
+        {
+            try
+            {
+                premis_view result = new premis_view();
+
+                result = await _dbContext.mst_premis.Where(x => x.gid == gid).Select(x => new premis_view
+                {
+                    gid = x.gid,
+                    lot = x.lot,
+                    status_cukai = x.tempoh_sah_cukai == null
+                                ? "None"
+                                : x.tempoh_sah_cukai > DateOnly.FromDateTime(DateTime.Now)
+                                    ? "Active"
+                                    : "Expired",
+                    status_lesen = x.tempoh_sah_lesen == null
+                                ? "None"
+                                : x.tempoh_sah_lesen > DateOnly.FromDateTime(DateTime.Now)
+                                    ? "Active"
+                                    : "Expired",
+                    no_cukai = GenerateRandomString(10),
+                    no_lesen = GenerateRandomString(8),
+                    nama_perniagaan = GenerateRandomString(15),
+                    nama_pemilik = GenerateRandomString(12),  
+                    alamat_premis1 = GenerateRandomString(30),
+                    alamat_premis2 = GenerateRandomString(30),
+                    status_notice = random.Next(0, 2) == 0 ? "Issued" : "Not Issued",
+                    status_expired_notice = random.Next(0, 2) == 0 ? "Yes" : "No",
+                    status_kompaun = random.Next(0, 2) == 0 ? "Paid" : "Unpaid",
+                    status_nota_pemeriksaan = random.Next(0, 2) == 0 ? "Passed" : "Failed"
+                }).AsNoTracking().FirstOrDefaultAsync();
+
+                result.lesen = new List<premis_license_view>();
+
+                result.lesen.Add(new premis_license_view
+                {
+                    aras = "G",
+                    status_lesen = result.status_lesen,
+                    no_lesen = GenerateRandomString(8),
+                    nama_perniagaan = GenerateRandomString(15),
+                    nama_pemilik = GenerateRandomString(12),
+                    alamat_premis = GenerateRandomString(30)
+                });
+
+                result.lesen.Add(new premis_license_view
+                {
+                    aras = "1",
+                    status_lesen = result.status_lesen,
+                    no_lesen = GenerateRandomString(8),
+                    nama_perniagaan = GenerateRandomString(15),
+                    nama_pemilik = GenerateRandomString(12),
+                    alamat_premis = GenerateRandomString(30)
+                });
+
+                result.lesen.Add(new premis_license_view
+                {
+                    aras = "2",
+                    status_lesen = result.status_lesen,
+                    no_lesen = GenerateRandomString(8),
+                    nama_perniagaan = GenerateRandomString(15),
+                    nama_pemilik = GenerateRandomString(12),
+                    alamat_premis = GenerateRandomString(30)
+                });
+
+                if (result == null)
+                {
+                    return NoContent(SystemMesg("COMMON", "EMPTY_DATA", MessageTypeEnum.Error, string.Format("Tiada rekod untuk dipaparkan")));
+                }
+
+                return Ok(result, SystemMesg(_feature, "LOAD_DATA", MessageTypeEnum.Success, string.Format("Data lot berjaya dijana")));
+            }
+            catch (Exception ex)
+            {
+                return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
+            }
+        }
+
+
         #region private logic
+        private static string GenerateRandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Range(0, length)
+                .Select(_ => chars[random.Next(chars.Length)])
+                .ToArray());
+        }
         #endregion
     }
 }
