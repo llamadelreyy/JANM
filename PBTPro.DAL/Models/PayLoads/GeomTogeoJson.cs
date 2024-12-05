d692599c04992eec304d4057906934b63bdbdb6d
Muhammad Ismail <spy.crzy@gmail.com>
2024-12-05 10:09:49 +0800

    sample api for get premis on point click

--- PBTPro.DAL/Models/PayLoads/GeomTogeoJson.cs
+++ PBTPro.DAL/Models/PayLoads/GeomTogeoJson.cs
@@ -13,7 +13,6 @@ namespace PBTPro.DAL.Models.PayLoads
         public int gid { get; set; }
         public double? objectid { get; set; }
         public string? lot { get; set; }
-
         public JsonDocument? geom { get; set; }
     }
 
@@ -21,9 +20,8 @@ namespace PBTPro.DAL.Models.PayLoads
     {
         public int gid { get; set; }
         public string? lot { get; set; }
-        public string? cukai_status { get; set; }
-        public string? lesen_status { get; set; }
-
+        public string? status_cukai { get; set; }
+        public string? status_lesen { get; set; }
         public JsonDocument? geom { get; set; }
     }
 }
