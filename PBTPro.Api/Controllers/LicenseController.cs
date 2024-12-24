85e4edaa4be9762afb97d135c10b4eca5a43016d
Nurulfarhana <DEVELOPER@CMSBPC02ACER>
2024-12-24 17:47:20 +0800

    24/12/2024

--- PBTPro.Api/Controllers/LicenseController.cs
+++ PBTPro.Api/Controllers/LicenseController.cs
@@ -36,29 +36,7 @@ namespace PBTPro.Api.Controllers
             _configuration = configuration;
         }
 
-        [AllowAnonymous]
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<license_view>>> ListAll()
-        {
-            try
-            {
-                var dt = await (from license_hist in _dbContext.license_histories
-                                join license_info in _dbContext.license_informations
-                                on license_hist.hist_id_info equals license_info.license_id
-                                group new { license_hist, license_info } by license_hist.hist_id_info into g
-                                select new
-                                {
-                                    Histories = g.Select(x => x.license_hist).ToList(),
-                                    LicenseInfo = g.Select(x => x.license_info).ToList()
-                                }).ToListAsync();
-
-                return Ok(dt, SystemMesg(_feature, "LOAD_DATA", MessageTypeEnum.Success, string.Format("Senarai rekod berjaya dijana")));
-            }
-            catch (Exception ex)
-            {
-                return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
-            }
-        }
+        
 
         [AllowAnonymous]
         [HttpGet]
