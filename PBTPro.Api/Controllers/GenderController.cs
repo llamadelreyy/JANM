9ffa2281b79b9cb797fa74e8276689332da1f72a
Nurulfarhana <DEVELOPER@CMSBPC02ACER>
2025-01-03 15:32:27 +0800

    api for core.reference

---
+++ PBTPro.Api/Controllers/GenderController.cs
@@ -0,0 +1,188 @@
+﻿/*
+Project: PBT Pro
+Description: Department API controller to handle department Form Field
+Author: Nurulfarhana
+Date: January 2025
+Version: 1.0
+Additional Notes:
+- 
+Changes Logs:
+*/
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using PBTPro.Api.Controllers.Base;
+using PBTPro.DAL;
+using PBTPro.DAL.Models;
+using PBTPro.DAL.Models.CommonServices;
+using PBTPro.DAL.Models.PayLoads;
+using PBTPro.DAL.Services;
+using System.Reflection;
+
+
+namespace PBTPro.Api.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class GenderController : IBaseController
+    {
+        protected readonly string? _dbConn;
+        private readonly IConfiguration _configuration;
+        private readonly IHubContext<PushDataHub> _hubContext;
+        private string LoggerName = "administrator";
+        private readonly string _feature = "GENDER";
+
+        public GenderController(IConfiguration configuration, PBTProDbContext dbContext, ILogger<GenderController> logger, IHubContext<PushDataHub> hubContext) : base(dbContext)
+        {
+            _dbConn = configuration.GetConnectionString("DefaultConnection");
+            _configuration = configuration;
+            _hubContext = hubContext;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ref_gender>>> ListAll()
+        {
+            try
+            {
+                var data = await _dbContext.ref_genders.AsNoTracking().ToListAsync();
+                return Ok(data, SystemMesg(_feature, "LOAD_DATA", MessageTypeEnum.Success, string.Format("Senarai rekod berjaya dijana")));
+            }
+            catch (Exception ex)
+            {
+                return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> ViewDetail(int Id)
+        {
+            try
+            {
+                var parFormfield = await _dbContext.ref_genders.FirstOrDefaultAsync(x => x.gen_id == Id);
+
+                if (parFormfield == null)
+                {
+                    return Error("", SystemMesg(_feature, "INVALID_RECID", MessageTypeEnum.Error, string.Format("Rekod tidak sah")));
+                }
+                return Ok(parFormfield, SystemMesg(_feature, "LOAD_DATA", MessageTypeEnum.Success, string.Format("Rekod berjaya dijana")));
+            }
+            catch (Exception ex)
+            {
+                return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] ref_gender InputModel)
+        {
+            try
+            {
+                var runUserID = await getDefRunUserId();
+                var runUser = await getDefRunUser();
+
+                #region store data
+                ref_gender ref_gender = new ref_gender
+                {
+                    gen_name = InputModel.gen_name,
+                    is_deleted = false,
+                    creator_id = runUserID,
+                    created_at = DateTime.Now,
+                };
+
+                _dbContext.ref_genders.Add(ref_gender);
+                await _dbContext.SaveChangesAsync();
+
+                #endregion
+
+                var result = new
+                {
+                    gen_name = ref_gender.gen_name,
+                    is_deleted = ref_gender.is_deleted,
+                    created_at = ref_gender.created_at
+                };
+                return Ok(result, SystemMesg(_feature, "CREATE", MessageTypeEnum.Success, string.Format("Berjaya cipta jadual rondaan")));
+            }
+            catch (Exception ex)
+            {
+                return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPut("{Id}")]
+        public async Task<IActionResult> Update(int Id, [FromBody] ref_gender InputModel)
+        {
+            try
+            {
+                int runUserID = await getDefRunUserId();
+                string runUser = await getDefRunUser();
+
+                #region Validation
+                var formField = await _dbContext.ref_genders.FirstOrDefaultAsync(x => x.gen_id == Id);
+                if (formField == null)
+                {
+                    return Error("", SystemMesg(_feature, "INVALID_RECID", MessageTypeEnum.Error, string.Format("Rekod tidak sah")));
+                }
+
+                if (string.IsNullOrWhiteSpace(InputModel.gen_name))
+                {
+                    return Error("", SystemMesg(_feature, "GENDER_NAME", MessageTypeEnum.Error, string.Format("Ruangan nama jantina diperlukan")));
+                }                
+
+                #endregion
+
+                formField.gen_name = InputModel.gen_name;
+                formField.is_deleted = InputModel.is_deleted;
+                formField.modifier_id = runUserID;
+                formField.modified_at = DateTime.Now;
+
+                _dbContext.ref_genders.Update(formField);
+                await _dbContext.SaveChangesAsync();
+
+                return Ok(formField, SystemMesg(_feature, "UPDATE", MessageTypeEnum.Success, string.Format("Berjaya mengubahsuai medan")));
+            }
+            catch (Exception ex)
+            {
+                return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> Delete(int Id)
+        {
+            try
+            {
+                string runUser = await getDefRunUser();
+
+                #region Validation
+                var formField = await _dbContext.ref_genders.FirstOrDefaultAsync(x => x.gen_id == Id);
+                if (formField == null)
+                {
+                    return Error("", SystemMesg(_feature, "INVALID_RECID", MessageTypeEnum.Error, string.Format("Rekod tidak sah")));
+                }
+                #endregion
+
+                _dbContext.ref_genders.Remove(formField);
+                await _dbContext.SaveChangesAsync();
+
+                return Ok(formField, SystemMesg(_feature, "REMOVE", MessageTypeEnum.Success, string.Format("Berjaya membuang medan")));
+            }
+            catch (Exception ex)
+            {
+                return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
+            }
+        }
+
+        private bool UnitExists(int id)
+        {
+            return (_dbContext.ref_genders?.Any(e => e.gen_id == id)).GetValueOrDefault();
+        }
+
+    }
+}
