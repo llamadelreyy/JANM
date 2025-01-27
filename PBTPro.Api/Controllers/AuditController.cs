9e45838b6b01efb9cb58f4b39791b37c173b6cb9
Nurulfarhana <DEVELOPER@CMSBPC02ACER>
2025-01-27 09:34:38 +0800

    25/1

--- PBTPro.Api/Controllers/AuditController.cs
+++ PBTPro.Api/Controllers/AuditController.cs
@@ -53,7 +53,7 @@ namespace PBTPro.Api.Controllers
         {            
             try
             {
-                var parFormfield = await _dbContext.auditlog_infos.FirstOrDefaultAsync(x => x.audit_id == Id);
+                var parFormfield = await _dbContext.auditlog_infos.FirstOrDefaultAsync(x => x.log_id == Id);
 
                 if (parFormfield == null)
                 {
@@ -77,7 +77,7 @@ namespace PBTPro.Api.Controllers
                 string runUser = await getDefRunUser();
 
                 #region Validation
-                var formField = await _dbContext.auditlog_infos.FirstOrDefaultAsync(x => x.audit_id == Id);
+                var formField = await _dbContext.auditlog_infos.FirstOrDefaultAsync(x => x.log_id == Id);
                 if (formField == null)
                 {
                     return Error("", SystemMesg(_feature, "INVALID_RECID", MessageTypeEnum.Error, string.Format("Rekod tidak sah")));
@@ -107,14 +107,14 @@ namespace PBTPro.Api.Controllers
                 #region store data
                 auditlog_info auditlog_info = new auditlog_info
                 {
-                    audit_role_id = InputModel.audit_role_id,
-                    audit_module_name = InputModel.audit_module_name,
-                    audit_description = InputModel.audit_description,
-                    audit_type = InputModel.audit_type,
-                    audit_username = InputModel.audit_username,
-                    audit_method = InputModel.audit_method,
-                    created_by = runUserID,
-                    created_date = DateTime.Now,
+                    role_id = InputModel.role_id,
+                    module_name = InputModel.module_name,
+                    log_descr = InputModel.log_descr,
+                    log_type = InputModel.log_type,
+                    username = InputModel.username,
+                    log_method = InputModel.log_method,
+                    creator_id = runUserID,
+                    created_at = DateTime.Now,
                 };
 
                 _dbContext.auditlog_infos.Add(auditlog_info);
@@ -124,14 +124,14 @@ namespace PBTPro.Api.Controllers
 
                 var result = new
                 {
-                    audit_role_id = auditlog_info.audit_role_id,
-                    audit_module_name = auditlog_info.audit_module_name,
-                    audit_description = auditlog_info.audit_description,
-                    audit_type = auditlog_info.audit_type,
-                    audit_username = auditlog_info.audit_username,
-                    audit_method = auditlog_info.audit_method,
-                    created_by = auditlog_info.created_by,
-                    created_date = auditlog_info.created_date
+                    role_id = auditlog_info.role_id,
+                    module_name = auditlog_info.module_name,
+                    log_descr = auditlog_info.log_descr,
+                    log_type = auditlog_info.log_type,
+                    username = auditlog_info.username,
+                    log_method = auditlog_info.log_method,
+                    creator_id = runUserID,
+                    created_at = DateTime.Now,
                 };
 
                 return Ok(result, SystemMesg(_feature, "INSERT_LOG_AUDIT", MessageTypeEnum.Success, string.Format("Berjaya tambah log audit baru.")));
@@ -170,78 +170,78 @@ namespace PBTPro.Api.Controllers
         #endregion
 
         #region unused
-        [AllowAnonymous]
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<auditlog_info>>> ListAuditwithParam(int skip, int? take, string? search)
-        {
-            if (_dbContext.auditlog_infos == null)
-            {
-                return NotFound();
-            }
-            var audits = await _dbContext.auditlog_infos
-               .Where(m =>
-               m.audit_description.Contains(search) ||
-               m.audit_username.Contains(search) ||
-               m.audit_method.Contains(search))
-               .ToListAsync();
+        //[AllowAnonymous]
+        //[HttpGet]
+        //public async Task<ActionResult<IEnumerable<auditlog_info>>> ListAuditwithParam(int skip, int? take, string? search)
+        //{
+        //    if (_dbContext.auditlog_infos == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    var audits = await _dbContext.auditlog_infos
+        //       .Where(m =>
+        //       m.log_descr.Contains(search) ||
+        //       m.username.Contains(search) ||
+        //       m.log_method.Contains(search))
+        //       .ToListAsync();
 
-            int totalRecord = audits.Count();
+        //    int totalRecord = audits.Count();
 
-            if (totalRecord == 0) return NoContent("Record not found");
+        //    if (totalRecord == 0) return NoContent("Record not found");
 
-            int takeRecord = take.HasValue ? take.Value : 10;
-            decimal tp = (decimal)totalRecord / (decimal)takeRecord;
-            int totalpage = (int)Math.Ceiling(tp);
+        //    int takeRecord = take.HasValue ? take.Value : 10;
+        //    decimal tp = (decimal)totalRecord / (decimal)takeRecord;
+        //    int totalpage = (int)Math.Ceiling(tp);
 
-            int page = (skip / takeRecord) + 1;
+        //    int page = (skip / takeRecord) + 1;
 
-            PaginationInfo paginationInfo = new PaginationInfo();
-            paginationInfo.TotalPages = totalpage;
-            paginationInfo.TotalRecords = totalRecord;
-            paginationInfo.RecordPerPage = takeRecord;
-            paginationInfo.CurrentPageNo = page;
+        //    PaginationInfo paginationInfo = new PaginationInfo();
+        //    paginationInfo.TotalPages = totalpage;
+        //    paginationInfo.TotalRecords = totalRecord;
+        //    paginationInfo.RecordPerPage = takeRecord;
+        //    paginationInfo.CurrentPageNo = page;
 
-            return Ok(audits, paginationInfo, "Sucess");
-            //return Ok(audits, "Sucess");
-            //return await _dbContext.TbAuditlogs.ToListAsync();
-        }
-        [AllowAnonymous]
-        [HttpPost]
-        public async Task<IActionResult> InsertAudits(auditlog_info changed, int intCurrentUserId)
-        {
-            try
-            {
-                var runUserID = await getDefRunUserId();
-                var runUser = await getDefRunUser();
+        //    return Ok(audits, paginationInfo, "Sucess");
+        //    //return Ok(audits, "Sucess");
+        //    //return await _dbContext.TbAuditlogs.ToListAsync();
+        //}
+        //[AllowAnonymous]
+        //[HttpPost]
+        //public async Task<IActionResult> InsertAudits(auditlog_info changed, int intCurrentUserId)
+        //{
+        //    try
+        //    {
+        //        var runUserID = await getDefRunUserId();
+        //        var runUser = await getDefRunUser();
 
-                using (NpgsqlConnection? myConn = new NpgsqlConnection(_dbConn))
-                {
-                    using (NpgsqlCommand? myCmd = new NpgsqlCommand("call audit.proc_insertaudit(:_audit_role_id, :_audit_module_name, :_audit_description, :_created_by, :_audit_type, :_audit_username, :_audit_method)", myConn))
-                    {
-                        myCmd.CommandType = CommandType.Text;
-                        myCmd.Parameters.AddWithValue("_audit_role_id", DbType.Int32).Value = changed.audit_role_id;
-                        myCmd.Parameters.AddWithValue("_audit_module_name", DbType.String).Value = changed.audit_module_name;
-                        myCmd.Parameters.AddWithValue("_audit_description", DbType.String).Value = changed.audit_description;
-                        myCmd.Parameters.AddWithValue("_created_by", DbType.Int32).Value = changed.created_by;
-                        myCmd.Parameters.AddWithValue("_audit_type", DbType.Int32).Value = changed.audit_type;
-                        myCmd.Parameters.AddWithValue("_audit_username", DbType.String).Value = changed.audit_username;
-                        myCmd.Parameters.AddWithValue("_audit_method", DbType.String).Value = changed.audit_method;
+        //        using (NpgsqlConnection? myConn = new NpgsqlConnection(_dbConn))
+        //        {
+        //            using (NpgsqlCommand? myCmd = new NpgsqlCommand("call audit.proc_insertlog(:_audit_role_id, :_audit_module_name, :_audit_description, :_created_by, :_audit_type, :_audit_username, :_audit_method)", myConn))
+        //            {
+        //                myCmd.CommandType = CommandType.Text;
+        //                myCmd.Parameters.AddWithValue("_audit_role_id", DbType.Int32).Value = changed.role_id;
+        //                myCmd.Parameters.AddWithValue("_audit_module_name", DbType.String).Value = changed.module_name;
+        //                myCmd.Parameters.AddWithValue("_audit_description", DbType.String).Value = changed.log_descr;
+        //                myCmd.Parameters.AddWithValue("_created_by", DbType.Int32).Value = changed.creator_id;
+        //                myCmd.Parameters.AddWithValue("_audit_type", DbType.Int32).Value = changed.log_type;
+        //                myCmd.Parameters.AddWithValue("_audit_username", DbType.String).Value = changed.username;
+        //                myCmd.Parameters.AddWithValue("_audit_method", DbType.String).Value = changed.log_method;
 
-                        myConn.Open();
-                        myCmd.ExecuteNonQuery();
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Exception caught : InsertAudit - {0}", ex);
-                return Ok("", SystemMesg(_feature, "INSERT_LOG_AUDIT", MessageTypeEnum.Success, string.Format("Berjaya tambah log audit baru.")));
-            }
-            finally
-            {
-            }
-            return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
-        }
+        //                myConn.Open();
+        //                myCmd.ExecuteNonQuery();
+        //            }
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine("Exception caught : LogAudit - {0}", ex);
+        //        return Ok("", SystemMesg(_feature, "INSERT_LOG_AUDIT", MessageTypeEnum.Success, string.Format("Berjaya tambah log audit baru.")));
+        //    }
+        //    finally
+        //    {
+        //    }
+        //    return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
+        //}
         #endregion
     }
 }
--- PBTPro.DAL/Models/auditlog_archive_info.cs
+++ PBTPro.DAL/Models/auditlog_archive_info.cs
@@ -6,18 +6,17 @@ namespace PBTPro.DAL.Models;
 
 public partial class auditlog_archive_info
 {
-    [Key]
     public int archive_id { get; set; }
 
-    public int archive_role_id { get; set; }
+    public int role_id { get; set; }
 
     public string archive_module_name { get; set; } = null!;
 
-    public string archive_description { get; set; } = null!;
+    public string archive_desc { get; set; } = null!;
 
-    public int? created_by { get; set; }
+    public int? creator_id { get; set; }
 
-    public DateTime? created_date { get; set; }
+    public DateTime? created_at { get; set; }
 
     public int? archive_type { get; set; }
 
@@ -25,8 +24,28 @@ public partial class auditlog_archive_info
 
     public string? archive_method { get; set; }
 
-    public bool? archive_isarchived { get; set; }
+    public bool? is_archived { get; set; }
+    //[Key]
+    //public int archive_id { get; set; }
 
-    public int archive_audit_id { get; set; }
+    //public int archive_role_id { get; set; }
+
+    //public string archive_module_name { get; set; } = null!;
+
+    //public string archive_description { get; set; } = null!;
+
+    //public int? created_by { get; set; }
+
+    //public DateTime? created_date { get; set; }
+
+    //public int? archive_type { get; set; }
+
+    //public string? archive_username { get; set; }
+
+    //public string? archive_method { get; set; }
+
+    //public bool? archive_isarchived { get; set; }
+
+    //public int archive_audit_id { get; set; }
 
 }
