cfd5ec5cb7fa27ac3710495b88db739f638fe191
Nurulfarhana <DEVELOPER@CMSBPC02ACER>
2025-02-14 14:13:07 +0800

    57(B): Access training material 056(B) : Access user manual

---
+++ PBTPro.Api/Controllers/DocumentController.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PBTPro.Api.Controllers.Base;
+using PBTPro.DAL;
+using PBTPro.DAL.Models;
+using PBTPro.DAL.Models.CommonServices;
+
+namespace PBTPro.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+
+    public class DocumentController : IBaseController
+    {
+        protected readonly string? _dbConn;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<DocumentController> _logger;
+        private readonly string _feature = "DOCUMENT_MATERIAL";
+
+        public DocumentController(IConfiguration configuration, PBTProDbContext dbContext, PBTProTenantDbContext tntdbContext, ILogger<DocumentController> logger) : base(dbContext)
+        {
+            _dbConn = configuration.GetConnectionString("DefaultConnection");
+            _configuration = configuration;
+            _logger = logger;
+            _tenantDBContext = tntdbContext;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ref_doc>>> ListAll()
+        {
+            try
+            {
+                var data = await _tenantDBContext.ref_docs.AsNoTracking().ToListAsync();
+                return Ok(data, SystemMesg(_feature, "LOAD_DATA", MessageTypeEnum.Success, string.Format("Senarai rekod berjaya dijana")));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(string.Format("{0} Message : {1}, Inner Exception {2}", _feature, ex.Message, ex.InnerException));
+                return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] ref_doc InputModel)
+        {
+            try
+            {
+                var runUserID = await getDefRunUserId();
+                var runUser = await getDefRunUser();
+
+                #region store data
+                ref_doc ref_doc = new ref_doc
+                {
+                    filename = InputModel.filename,
+                    pathurl = InputModel.pathurl,
+                    doc_cat = InputModel.doc_cat,
+                    creator_id = runUserID,
+                    created_at = DateTime.Now,
+                };
+
+                _tenantDBContext.ref_docs.Add(ref_doc);
+                await _tenantDBContext.SaveChangesAsync();
+
+                #endregion
+
+                var result = new
+                {
+                    filename = ref_doc.filename,
+                    pathurl = ref_doc.pathurl,
+                    doc_cat = ref_doc.doc_cat,
+                    created_at = ref_doc.created_at
+                };
+                return Ok(result, SystemMesg(_feature, "CREATE", MessageTypeEnum.Success, string.Format("Berjaya tambah data.")));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(string.Format("{0} Message : {1}, Inner Exception {2}", _feature, ex.Message, ex.InnerException));
+                return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpPut("{Id}")]
+        public async Task<IActionResult> Update(int Id, [FromBody] ref_doc InputModel)
+        {
+            try
+            {
+                int runUserID = await getDefRunUserId();
+                string runUser = await getDefRunUser();
+
+                #region Validation
+                var formField = await _tenantDBContext.ref_docs.FirstOrDefaultAsync(x => x.doc_id == InputModel.doc_id);
+                if (formField == null)
+                {
+                    return Error("", SystemMesg(_feature, "INVALID_RECID", MessageTypeEnum.Error, string.Format("Rekod tidak sah")));
+                }
+
+                if (string.IsNullOrWhiteSpace(InputModel.filename))
+                {
+                    return Error("", SystemMesg(_feature, "FILE_NAME", MessageTypeEnum.Error, string.Format("Ruangan nama bangsa diperlukan")));
+                }
+
+                #endregion
+
+                formField.filename = InputModel.filename;
+                formField.pathurl = InputModel.pathurl;
+                formField.doc_cat = InputModel.doc_cat;
+                formField.modifier_id = runUserID;
+                formField.modified_at = DateTime.Now;
+
+                _tenantDBContext.ref_docs.Update(formField);
+                await _tenantDBContext.SaveChangesAsync();
+
+                return Ok(formField, SystemMesg(_feature, "UPDATE", MessageTypeEnum.Success, string.Format("Berjaya mengubahsuai medan")));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(string.Format("{0} Message : {1}, Inner Exception {2}", _feature, ex.Message, ex.InnerException));
+                return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> Delete(int Id)
+        {
+            try
+            {
+                string runUser = await getDefRunUser();
+
+                #region Validation
+                var formField = await _tenantDBContext.ref_docs.FirstOrDefaultAsync(x => x.doc_id == Id);
+                if (formField == null)
+                {
+                    return Error("", SystemMesg(_feature, "INVALID_RECID", MessageTypeEnum.Error, string.Format("Rekod tidak sah")));
+                }
+                #endregion
+
+                _tenantDBContext.ref_docs.Remove(formField);
+                await _tenantDBContext.SaveChangesAsync();
+
+                return Ok(formField, SystemMesg(_feature, "REMOVE", MessageTypeEnum.Success, string.Format("Berjaya membuang medan")));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(string.Format("{0} Message : {1}, Inner Exception {2}", _feature, ex.Message, ex.InnerException));
+                return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> ViewDetail(int Id)
+        {
+            try
+            {
+                var parFormfield = await _tenantDBContext.ref_docs.FirstOrDefaultAsync(x => x.doc_id == Id);
+
+                if (parFormfield == null)
+                {
+                    return Error("", SystemMesg(_feature, "INVALID_RECID", MessageTypeEnum.Error, string.Format("Rekod tidak sah")));
+                }
+                return Ok(parFormfield, SystemMesg(_feature, "LOAD_DATA", MessageTypeEnum.Success, string.Format("Rekod berjaya dijana")));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(string.Format("{0} Message : {1}, Inner Exception {2}", _feature, ex.Message, ex.InnerException));
+                return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
+            }
+        }
+
+    }
+}
