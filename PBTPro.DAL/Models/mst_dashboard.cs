4c0a3fcab7656845e8581ca4e9c97ab38d8ff863
Azmee <azmeecherose@gmail.com>
2025-02-18 14:45:31 +0800

    18/2 - Part 1

---
+++ PBTPro.DAL/Models/mst_dashboard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PBTPro.DAL.Models;
+
+public partial class mst_dashboard
+{
+    public int JumlahNotisSP { get; set; }
+    public int JumlahPremisBerlesenSP { get; set; }
+    public int JumlahNotis { get; set; }
+    public int JumlahKompaun { get; set; }
+    public int JumlahNotaPemeriksaan { get; set; }
+    public int JumlahSitaan { get; set; }
+    public int JumlahPremisBerlesen { get; set; }
+    public int JumlahPremisTidakBerlesen { get; set; }
+    public int JumlahLesenAktif { get; set; }
+    public int JumlahLesenTamatTempoh { get; set; }
+    public int JumlahLesenGantung { get; set; }
+    public int JumlahLesenTiadaData { get; set; }
+    public int PertambahanLesenTahunSemasa { get; set; }
+    public int PertambahanLesenBulanSemasa { get; set; }
+
+}
