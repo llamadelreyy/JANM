189a743859f7f77c66ef96c0cdd1c7bc536fd26f
Nurulfarhana <DEVELOPER@CMSBPC02ACER>
2025-02-19 14:38:25 +0800

    #Dashboard API : cukai_taksiran_dibyr, cukai_taksiran_blm_dibyr, hsl_lesen_dibyr, hsl_lesen_blm_dibyr, kompaun_dibyr, kompaun_blm_dibyr

--- PBTPro.DAL/Models/PayLoads/dashboard_view.cs
+++ PBTPro.DAL/Models/PayLoads/dashboard_view.cs
@@ -18,7 +18,14 @@ namespace PBTPro.DAL.Models.PayLoads
         public int premis_dikenakan_tindakan { get; set; }
         public int premis_tiada_lesen { get; set; }
         public int total_cukai_tahunan { get; set; }
-        public double amaun_kutipan_cukai { get; set; }
+        public decimal amaun_kutipan_cukai { get; set; }
+        public decimal cukai_taksiran_dibyr { get; set; }
+        public decimal cukai_taksiran_blm_dibyr {get; set;}
+        public decimal hsl_lesen_dibyr { get; set; }
+        public decimal hsl_lesen_blm_dibyr { get; set; }
+        public decimal? kompaun_dibyr { get; set; }
+        public decimal? kompaun_blm_dibyr { get; set; }
+
 
     }
 }
