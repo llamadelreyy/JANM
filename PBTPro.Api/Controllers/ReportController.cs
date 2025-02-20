9147dccdf40efd066aba641a228e3ddd764f3641
Nurulfarhana <DEVELOPER@CMSBPC02ACER>
2025-02-20 17:15:38 +0800

    API : ReportLesen DAL : report_view

--- PBTPro.Api/Controllers/ReportController.cs
+++ PBTPro.Api/Controllers/ReportController.cs
@@ -51,10 +51,13 @@ namespace PBTPro.Api.Controllers
 
         [AllowAnonymous]
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<report_view>>> ReportLesen(int opsid=0, int catid=0)
+        public async Task<ActionResult<IEnumerable<report_view>>> ReportLesen(int opsid = 0, int catid = 0, DateTime? startDate = null, DateTime? endDate = null)
         {
             try
             {
+                startDate ??= DateTime.Today;
+                endDate ??= DateTime.Today.AddMonths(1);
+
                 var data = await _tenantDBContext.mst_licensees.Where(e => e.is_deleted == false)
                             .Select(e => new mst_licensee
                             {
@@ -86,10 +89,14 @@ namespace PBTPro.Api.Controllers
 
                 var result = (from license in data
                               join category in _tenantDBContext.ref_license_cats on license.cat_id equals category.cat_id into kategoriGroup
-                              from category in kategoriGroup.DefaultIfEmpty() 
+                              from category in kategoriGroup.DefaultIfEmpty()
                               join ops in _tenantDBContext.ref_license_ops on license.ops_id equals ops.ops_id into opsGroup
                               from ops in opsGroup.DefaultIfEmpty()
-                              where license.ops_id == opsid || license.cat_id == catid
+                              where (license.ops_id == opsid || opsid == 0)
+                                     && (license.cat_id == catid || catid == 0)
+                                     && (startDate == null || license.created_at >= startDate)
+                                     && (endDate == null || license.created_at <= endDate)
+
                               select new report_view
                               {
                                   report_id = license.licensee_id,
@@ -97,8 +104,131 @@ namespace PBTPro.Api.Controllers
                                   alamat = license.business_addr,
                                   no_lesen = license.license_accno,
                                   no_ssm = license.ssm_no,
-                                  operasi_lesen = ops != null ? ops.ops_name : null, 
+                                  operasi_id = ops != null ? ops.ops_id : 0,
+                                  operasi_lesen = ops != null ? ops.ops_name : null,
+                                  kategori_id = category != null ? category.cat_id : 0,
                                   kategori_lesen = category != null ? category.cat_name : null,
+                                  created_at = (DateTime)license.created_at,
+                              }
+                            ).ToList();
+
+                return Ok(result, SystemMesg(_feature, "LOAD_DATA", MessageTypeEnum.Success, string.Format("Senarai rekod berjaya dijana")));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(string.Format("{0} Message : {1}, Inner Exception {2}", _feature, ex.Message, ex.InnerException));
+                return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<report_view>>> ReportPremisByParlimen(int parlid = 0, DateTime? startDate = null, DateTime? endDate = null)
+        {
+            try
+            {
+                startDate ??= DateTime.Today;
+                endDate ??= DateTime.Today.AddMonths(1);
+
+                IQueryable<mst_premis> data = (IQueryable<mst_premis>)_dbContext.mst_premis.ToListAsync();
+                             
+                var result = (from premis in data                              
+                              //where (premis.ops_id == parlid || parlid == 0)                                    
+                              //       && (startDate == null || premis.created_at >= startDate)
+                              //       && (endDate == null || premis.created_at <= endDate)
+
+                              select new report_view
+                              {
+                                  //report_id = license.licensee_id,
+                                  //nama_premis = license.business_name,
+                                  //alamat = license.business_addr,
+                                  //no_lesen = license.license_accno,
+                                  //no_ssm = license.ssm_no,
+                                  //operasi_id = ops != null ? ops.ops_id : 0,
+                                  //operasi_lesen = ops != null ? ops.ops_name : null,
+                                  //kategori_id = category != null ? category.cat_id : 0,
+                                  //kategori_lesen = category != null ? category.cat_name : null,
+                                  //created_at = (DateTime)license.created_at,
+                              }
+                            ).ToList();
+
+                return Ok(result, SystemMesg(_feature, "LOAD_DATA", MessageTypeEnum.Success, string.Format("Senarai rekod berjaya dijana")));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(string.Format("{0} Message : {1}, Inner Exception {2}", _feature, ex.Message, ex.InnerException));
+                return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<report_view>>> ReportPremisByDun(int dunid = 0, DateTime? startDate = null, DateTime? endDate = null)
+        {
+            try
+            {
+                startDate ??= DateTime.Today;
+                endDate ??= DateTime.Today.AddMonths(1);
+
+                IQueryable<mst_premis> data = (IQueryable<mst_premis>)_dbContext.mst_premis.ToListAsync();
+
+                var result = (from premis in data
+                                  //where (premis.ops_id == parlid || parlid == 0)                                    
+                                  //       && (startDate == null || premis.created_at >= startDate)
+                                  //       && (endDate == null || premis.created_at <= endDate)
+
+                              select new report_view
+                              {
+                                  //report_id = license.licensee_id,
+                                  //nama_premis = license.business_name,
+                                  //alamat = license.business_addr,
+                                  //no_lesen = license.license_accno,
+                                  //no_ssm = license.ssm_no,
+                                  //operasi_id = ops != null ? ops.ops_id : 0,
+                                  //operasi_lesen = ops != null ? ops.ops_name : null,
+                                  //kategori_id = category != null ? category.cat_id : 0,
+                                  //kategori_lesen = category != null ? category.cat_name : null,
+                                  //created_at = (DateTime)license.created_at,
+                              }
+                            ).ToList();
+
+                return Ok(result, SystemMesg(_feature, "LOAD_DATA", MessageTypeEnum.Success, string.Format("Senarai rekod berjaya dijana")));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(string.Format("{0} Message : {1}, Inner Exception {2}", _feature, ex.Message, ex.InnerException));
+                return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
+            }
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<report_view>>> ReportPremisByZon(int zonid = 0, DateTime? startDate = null, DateTime? endDate = null)
+        {
+            try
+            {
+                startDate ??= DateTime.Today;
+                endDate ??= DateTime.Today.AddMonths(1);
+
+                IQueryable<mst_premis> data = (IQueryable<mst_premis>)_dbContext.mst_premis.ToListAsync();
+
+                var result = (from premis in data
+                                  //where (premis.ops_id == parlid || parlid == 0)                                    
+                                  //       && (startDate == null || premis.created_at >= startDate)
+                                  //       && (endDate == null || premis.created_at <= endDate)
+
+                              select new report_view
+                              {
+                                  //report_id = license.licensee_id,
+                                  //nama_premis = license.business_name,
+                                  //alamat = license.business_addr,
+                                  //no_lesen = license.license_accno,
+                                  //no_ssm = license.ssm_no,
+                                  //operasi_id = ops != null ? ops.ops_id : 0,
+                                  //operasi_lesen = ops != null ? ops.ops_name : null,
+                                  //kategori_id = category != null ? category.cat_id : 0,
+                                  //kategori_lesen = category != null ? category.cat_name : null,
+                                  //created_at = (DateTime)license.created_at,
                               }
                             ).ToList();
 
