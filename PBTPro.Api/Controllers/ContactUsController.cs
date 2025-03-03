feda1992cc0b252332d10208dac1a0a459bd563f
Nurulfarhana <DEVELOPER@CMSBPC02ACER>
2025-03-03 11:27:18 +0800

    #034 - reopen ticket archive

--- PBTPro.Api/Controllers/ContactUsController.cs
+++ PBTPro.Api/Controllers/ContactUsController.cs
@@ -270,39 +270,39 @@ namespace PBTPro.Api.Controllers
             }
         }
 
-        [AllowAnonymous]
-        [HttpGet]
-        public async Task<IActionResult> TotalCOntactus()
-        {
-            try
-            {
-                using (NpgsqlConnection? myConn = new NpgsqlConnection(_dbConn))
-                {
-                    using (NpgsqlCommand? myCmd = new NpgsqlCommand("SELECT core.func_totalcontactus()", myConn))
-                    {
-                        myConn.Open();
+        //[AllowAnonymous]
+        //[HttpGet]
+        //public async Task<IActionResult> TotalCOntactus()
+        //{
+        //    try
+        //    {
+        //        using (NpgsqlConnection? myConn = new NpgsqlConnection(_dbConn))
+        //        {
+        //            using (NpgsqlCommand? myCmd = new NpgsqlCommand("SELECT core.func_totalcontactus()", myConn))
+        //            {
+        //                myConn.Open();
 
-                        var total = myCmd.ExecuteScalar();
+        //                var total = myCmd.ExecuteScalar();
 
-                        if (total == null)
-                        {
-                            return Error("", SystemMesg("COMMON", "NO_DATA", MessageTypeEnum.Error, "No data found."));
-                        }
-                        int totalCount = Convert.ToInt32(total);
+        //                if (total == null)
+        //                {
+        //                    return Error("", SystemMesg("COMMON", "NO_DATA", MessageTypeEnum.Error, "No data found."));
+        //                }
+        //                int totalCount = Convert.ToInt32(total);
 
-                        return Ok(new { totalCount = totalCount }, SystemMesg(_feature, "LOAD_DATA", MessageTypeEnum.Success, "Senarai rekod berjaya dijana"));
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(string.Format("{0} Message : {1}, Inner Exception {2}", _feature, ex.Message, ex.InnerException));
-                return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
-            }
-            finally
-            {
-            }
-        }
+        //                return Ok(new { totalCount = totalCount }, SystemMesg(_feature, "LOAD_DATA", MessageTypeEnum.Success, "Senarai rekod berjaya dijana"));
+        //            }
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _logger.LogError(string.Format("{0} Message : {1}, Inner Exception {2}", _feature, ex.Message, ex.InnerException));
+        //        return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
+        //    }
+        //    finally
+        //    {
+        //    }
+        //}
         static string GenerateRandomString(int length)
         {
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
