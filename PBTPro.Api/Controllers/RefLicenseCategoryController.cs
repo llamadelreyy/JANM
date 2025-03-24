040645192a9665f12df80f6ea6779dd67edc4280
Muhammad Ismail <spy.crzy@gmail.com>
2025-03-24 08:47:25 +0800

    add get detail - license category - license type - notice duration

--- PBTPro.Api/Controllers/RefLicenseCategoryController.cs
+++ PBTPro.Api/Controllers/RefLicenseCategoryController.cs
@@ -59,6 +59,26 @@ namespace PBTPro.Api.Controllers
                 return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
             }
         }
-           
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetDetail(int Id)
+        {
+            try
+            {
+                var ref_license_cat = await _tenantDBContext.ref_license_cats.FirstOrDefaultAsync(x => x.cat_id == Id);
+
+                if (ref_license_cat == null)
+                {
+                    return Error("", SystemMesg(_feature, "INVALID_RECID", MessageTypeEnum.Error, string.Format("Rekod tidak sah")));
+                }
+
+                return Ok(ref_license_cat, SystemMesg(_feature, "LOAD_DATA", MessageTypeEnum.Success, string.Format("Rekod berjaya dijana")));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(string.Format("{0} Message : {1}, Inner Exception {2}", _feature, ex.Message, ex.InnerException));
+                return Error("", SystemMesg("COMMON", "UNEXPECTED_ERROR", MessageTypeEnum.Error, string.Format("Maaf berlaku ralat yang tidak dijangka. sila hubungi pentadbir sistem atau cuba semula kemudian.")));
+            }
+        }
     }
 }
