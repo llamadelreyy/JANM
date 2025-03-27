66c41d192cebcdfe81134b26e295b8d3aa740e0b
Nurulfarhana <DEVELOPER@CMSBPC02ACER>
2025-03-27 12:11:52 +0800

    minor fix ticket 71(b) & 85

--- PBTPro.DAL/Models/PayLoads/trn_compound_view.cs
+++ PBTPro.DAL/Models/PayLoads/trn_compound_view.cs
@@ -98,4 +98,41 @@ namespace PBTPro.DAL.Models.PayLoads
         public decimal? inspect_latitude { get; set; }
         public bool is_cukai { get; set; }
     }
+
+    public class trn_cfsc_view
+    {
+        public int id_sita { get; set; }
+        public string? no_rujukan { get; set; }
+        public int? lesen_id { get; set; }
+        public string? no_lesen { get; set; }
+        public string? nama_perniagaan { get; set; }
+        public string? nama_pemilik { get; set; }
+        public string? nama_pegawai { get; set; }
+        public string? ssm_no { get; set; }
+        public string? alamat_perniagaan { get; set; }
+        public string? arahan { get; set; }
+        public string? lokasi_kesalahan { get; set; }
+        public DateTime? TarikhData { get; set; }
+        public string? nama_dokumen { get; set; }
+        public string? pautan_dokumen { get; set; }
+        public string? no_cukai { get; set; }
+        public List<string> imej_sita { get; set; }
+        public string? nama_jabatan { get; set; }
+        public int status_sita_id { get; set; }
+        public string? status_sita { get; set; }
+        public decimal? inspect_longitude { get; set; }
+        public decimal? inspect_latitude { get; set; }
+        public bool is_cukai { get; set; }
+        public string? akta_kesalahan { get; set; }
+        public string? kod_kesalahan { get; set; }
+        public string? kod_seksyen { get; set; }
+        public string? kod_uuk { get; set; }
+        public int? inv_id { get; set; }
+        public int? inv_type_id { get; set; }
+        public int? scenario_id { get; set; }
+        public string? nama_scenario { get; set; }
+        public string? nama_inventori { get; set; }
+        public int? jenis_inventori { get; set; }
+        public string? nama_inv_type { get; set; }
+    }
 }
